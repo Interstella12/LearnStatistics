01b58324576a3166c15b816acfe04a628315470f
Interstella12 <lccever@126.com>
2019-11-04 11:30:17 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @time    : 2019/10/30 上午11:15
+# @author  : jinggui
+# @email   : lccever@126.com
+# @file    : test.py
+# @software: LearnStatistics
+# @desc : ==============================================
+# Life is short, I use python.                       ===
+# If this runs wrong, don't ask me, I don't know why ===
+# If this runs right, thank god, and I don't know why===
+# Maybe the answer, my friend, is blowing in the wind===
+# ======================================================
+
+
+import numpy as np
+import pandas as pd
+
+d = pd.DataFrame()
+d.skew
